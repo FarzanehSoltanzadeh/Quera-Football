155ba68d35d8cb9d265715e96af8357d5c5eb2d9
Farzaneh <71216743+FarzanehSoltanzadeh@users.noreply.github.com>
2023-05-24 05:28:56 +0430

    club and player extraction

--- Phase1/crawl_club.py
+++ Phase1/crawl_club.py
@@ -1,7 +1,15 @@
+__author__ = "Farzaneh Soltanzadeh, Ali Mousavi"
+__email__ = "f.soltanzadeh.f@gmail.com, ali.mousavi4878@gmail.com"
+__mentor__ = "Diba Aminshahidi"
+__organization__ = "Quera"
+__date__ = "2023-05-24"
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+import re
+
 """ 
 England : 189
 Germany: 40
@@ -27,7 +35,6 @@ for i in range(len(club)):
     soup = BeautifulSoup(page.content, 'html.parser')
     league.append(soup.select('.inline-table a')[1].get('title'))
 
-
 league_urls = {'Premier League': 'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1/plus/?saison_id=',
                'Bundesliga': 'https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1/plus/?saison_id=',
                'Serie A': 'https://www.transfermarkt.com/serie-a/startseite/wettbewerb/IT1/plus/?saison_id=',
@@ -35,7 +42,7 @@ league_urls = {'Premier League': 'https://www.transfermarkt.com/premier-league/s
                'LaLiga': 'https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1/plus/?saison_id='}
 
 season = [[i for i in range(2015, 2022)] for j in range(len(club))]
-
+All_Players_Link = []
 
 
 club['League'] = league
@@ -45,7 +52,8 @@ club = club.assign(Club=np.NaN, ClubID=np.NaN, Squad=np.NaN,
                    avgAge=np.NaN, Foreigners=np.NaN, avgMarketValue=np.NaN, 
                    totalMarketValue=np.NaN, Stadium=np.NaN, StadiumCap=np.NaN,
                    Club_income=np.NaN, Club_expenditure=np.NaN, Club_OverallBalance=np.NaN, 
-                   Coach=np.NaN, Players=np.NaN)
+                   Coach=np.NaN, Club_victories=np.NaN, Players=np.NaN)
+
 
 club_table_league['League'] = league
 club_table_league['Season'] = season
@@ -57,7 +65,7 @@ players = pd.DataFrame()
 players['League'] = league
 players['Season'] = season
 players = players.explode('Season').reset_index(drop=True)
-players.assign(ClubID=np.NaN, Player_name=np.NaN, Player_MarketValue=np.NaN, Player_possition=np.NaN)
+players.assign(ClubID=np.NaN, Player_name=np.NaN, PlayerID=np.NaN, Player_MarketValue=np.NaN, Player_possition=np.NaN)
 
 
 for league, url in league_urls.items():
@@ -65,86 +73,127 @@ for league, url in league_urls.items():
         page = requests.get(url + str(season), headers={'User-Agent': 'Mozilla/5.0'})
         soup = BeautifulSoup(page.content, 'html.parser')
 
-
+        # add Rank and Club name columns to club_table_league dataFrame
         top_team = [team.text for team in soup.select("#yw4 .hauptlink a:nth-child(1)")] 
         club_table_league.loc[(club_table_league['Season'] == season) & (club_table_league['League'] == league), 'Club'] = ';'.join(top_team)
         club_table_league['Club'] = club_table_league['Club'].str.split(';')
         club_table_league = club_table_league.explode('Club').reset_index(drop=True)
         club_table_league.loc[(club_table_league['Season'] == season) & (club_table_league['League'] == league), 'Rank'] = [i + 1 for i in range(len(top_team))]
         
-                                                  
+        # get Clubs                                         
         team = soup.select('#yw1 .no-border-links a:nth-child(1)')
-
+        
+        # add ClubID to players dataFrame
         players.loc[(players['Season'] == season) & (players['League'] == league), 'ClubID'] = ';'.join(t.get('href').split("/")[-3] for t in team)
         players['ClubID'] = players['ClubID'].str.split(';')
         players = players.explode('ClubID').reset_index(drop=True)
         
-
+        # add ClubID and Club name to club dataFrame
         club.loc[(club['Season'] == season) & (club['League'] == league), 'Club'] = ';'.join(t.get('title') for t in team)
         club['Club'] = club['Club'].str.split(';')
         club.loc[(club['Season'] == season) & (club['League'] == league), 'ClubID'] = ';'.join(t.get('href').split("/")[-3] for t in team)
         club['ClubID'] = club['ClubID'].str.split(';')
         club = club.explode(['Club', 'ClubID']).reset_index(drop=True)
         
-
+        # add Squad column to club dataFrame 
         squad = [squad.text for squad in soup.select(".no-border-links+ .zentriert > a")] 
         club.loc[(club['Season'] == season) & (club['League'] == league), 'Squad'] = squad
         
+        # add aveAge to club dataFrame 
         age = [age.text for age in soup.select("#yw1 tbody .zentriert:nth-child(4)")]
         club.loc[(club['Season'] == season) & (club['League'] == league), 'avgAge'] = age
     
+        # add foreigner count to club dataFrame 
         foreigner = [number_Forigners.text for number_Forigners in soup.select("#yw1 tbody .zentriert:nth-child(5)")]
         club.loc[(club['Season'] == season) & (club['League'] == league), 'Foreigners'] = foreigner
 
+        # add avgMarketValue to club dataFrame 
         avrage_market_value = [market_value.text for market_value in soup.select("tbody .zentriert+ .rechts")]
         club.loc[(club['Season'] == season) & (club['League'] == league), 'avgMarketValue'] = avrage_market_value
- 
+
+        # add totalMarketValue to club dataFrame 
         total_market_value = [total.text for total in soup.select("tbody .rechts+ .rechts")]
         club.loc[(club['Season'] == season) & (club['League'] == league), 'totalMarketValue'] = total_market_value
         
+        # trace on the players page
         club_urls = [link.get("href") for link in soup.select("#yw1 .no-border-links a:nth-child(1)")]
         for club_url in club_urls:
             club_id = club_url.split("/")[-3]
             club_page = requests.get("https://www.transfermarkt.com" + club_url, headers={'User-Agent': 'Mozilla/5.0'})  
             soup2 = BeautifulSoup(club_page.content, "html.parser")
 
-            players_list = [player.text.split('.')[0][:-1] for player in soup2.select(".inline-table .hauptlink")]
-            players.loc[(players['ClubID'] == club_id) & (players['Season'] == season), 'Player_name'] = ';'.join(players_list)
-            players['Player_name'] = players['Player_name'].str.split(';')
-            players = players.explode('Player_name').reset_index(drop=True)
+            # get all players from the club
+            players_list = soup2.select(".inline-table .hauptlink .hide-for-small > a")
+            # get all players url from the club
+            All_Players_Link.extend(p.get('href') for p in players_list)
             
+            # add player name and player id to player dataFrame
+            players.loc[(players['ClubID'] == club_id) & (players['Season'] == season), 'Player_name'] = ';'.join(p.text for p in players_list)
+            players.loc[(players['ClubID'] == club_id) & (players['Season'] == season), 'PlayerID'] = ';'.join(p.get('href').split('/')[-1] for p in players_list)
+            players['Player_name'] = players['Player_name'].str.split(';')
+            players['PlayerID'] = players['PlayerID'].str.split(';')
+            players = players.explode(['Player_name', 'PlayerID']).reset_index(drop=True)
+
+            # add player Market Value to player dataFrame
             player_market_value = [pmv.text for pmv in soup2.select(".rechts.hauptlink")]
             players.loc[(players['ClubID'] == club_id) & (players['Season'] == season), 'Player_MarketValue'] = player_market_value
             
+            # add player possition to player dataFrame
             player_main_possition = [possition.text for possition in soup2.select(".inline-table tr+ tr td")]
             players.loc[(players['ClubID'] == club_id) & (players['Season'] == season), 'Player_possition'] = player_main_possition
         
-
+            # add stadium name and stadium capacity to club dataFrame
             stadium_name = [stadium.text for stadium in soup2.select(".data-header__items+ .data-header__items .data-header__label+ .data-header__label .data-header__content > a")]
             club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Stadium'] = stadium_name
             stadium_capacity = [capacity.text for capacity in soup2.select(".data-header__items+ .data-header__items .tabellenplatz")]
             club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'StadiumCap'] = stadium_capacity
 
-            club_income = [income.text.replace("\n", "") for income in soup2.select(".transfer-record__total--positive")]
+            # add club income to club dataFrame
+            club_income = [income.text.replace("\n", "").replace(' ','') for income in soup2.select(".transfer-record__total--positive")]
             club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Club_income'] = club_income
             
-            club_expenditure = [Expenditure.text.replace("\n", "") for Expenditure in soup2.select(".transfer-record__total--negative")]
+            # add club expenditure to club dataFrame
+            club_expenditure = [Expenditure.text.replace("\n", "").replace(' ','') for Expenditure in soup2.select(".transfer-record__total--negative")]
             club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Club_expenditure'] = club_expenditure
 
-            club_OverallBalance = [Overall_balance.text.replace("\n", "").replace(' ','') for Overall_balance in soup2.select(".rechts.transfer-record__total")]
-            club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Club_OverallBalance'] = club_OverallBalance
+            # add club overallBalance to club dataFrame
+            club_overallBalance = [Overall_balance.text.replace("\n", "").replace(' ','') for Overall_balance in soup2.select(".rechts.transfer-record__total")]
+            club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Club_OverallBalance'] = club_overallBalance
 
+            # add coach name to club dataFrame
             coach_list = set(coach.text for coach in soup2.select(".staff-slider-main , .container-main > a"))
             club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Coach'] = ';'.join(coach_list)
             
-            club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Players'] = ';'.join(players_list)
+            # add players name to club dataFrame
+            club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Players'] = ';'.join(p.text for p in players_list)
 
-
-        print('\t',season, 'Done.')
+            # add club victories to club dataFrame
+            champoin = [champion.get("href") for champion in soup2.select(".data-header__badge-container > a")]
+            if not champoin:
+                continue
+            
+            champoin_page = requests.get("https://www.transfermarkt.com" + champoin[0], headers={'User-Agent': 'Mozilla/5.0'})
+            soup3 = BeautifulSoup(champoin_page.content, "html.parser")
+            
+            champoin_list = [ch.text for ch in soup3.select("td:nth-child(1) , .no-border-links")]
+            victories = []
+           
+            for i in range(0, len(champoin_list), 2):
+                year, name = champoin_list[i].split('/')[0], champoin_list[i+1]
+                if len(year) == 2 and 1 <= int(year[0]) <= 2:
+                    if int('20' + year) == season:
+                        victories.append(name)
+            
+            club.loc[(club['ClubID'] == club_id) & (club['Season'] == season), 'Club_victories'] = ';'.join(victories)
+     
+            
+        print('\t',season, 'Done.')  
     print(league,'Done.')
 
+   
 
+club.to_csv('data/club.csv')
+club_table_league.to_csv('data/club_table_league.csv')
+players.to_csv('data/club_players.csv')
+pd.DataFrame(All_Players_Link).drop_duplicates().to_csv("data/players_link.csv", index = False)
 
-club.to_csv('club32.csv')
-club_table_league.to_csv('club_table_league32.csv')
-players.to_csv('players32.csv')
---
+++ Phase1/crawl_player.py
@@ -0,0 +1,277 @@
+__author__ = "Zahra Honarvar, Milad Nooraei"
+__email__ = "Zhpica@gmail.com, miladnooraiy0@gmail.com"
+__mentor__ = "Diba Aminshahidi"
+__organization__ = "Quera"
+__date__ = "2023-05-24"
+
+import re
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import numpy as np
+
+
+def data_of_players(url, players_datas):      
+    page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(page.content, "html.parser")
+    res = {} 
+
+    #Getting Players ID
+    pattern = r"/(\d+)$"
+    match = re.search(pattern, url)
+    player_id = match.group(1)
+    res["player_id"] = player_id
+
+    #Getting Given Name and Shirt Number
+    try:
+        header = soup.find("h1", class_ = "data-header__headline-wrapper")
+        shirt_number = header.find("span", class_ = "data-header__shirt-number").get_text(strip = True)
+        given_name = " ".join(header.stripped_strings).replace(shirt_number, "").strip()
+    except AttributeError:
+        shirt_number = None
+        given_name = None
+    if given_name == None:
+        try:
+            given_name = soup.find("h1", class_ = "data-header__headline-wrapper").text.strip()
+        except AttributeError:
+            given_name = None
+    res["shirt_number"] = shirt_number
+    res["given_name"] = given_name
+
+    #Getting Full Name
+    try:
+        full_name = soup.select_one("span.info-table__content.info-table__content--bold").get_text(strip = True)
+        pattern = r"\d+"
+        match = re.search(pattern, full_name)
+        if(bool(match)):
+            full_name = None
+    except AttributeError:
+        full_name = None
+    res["full_name"] = full_name
+
+    #Getting Date Of Birth
+    try:
+        date_of_birth = soup.select_one("span.info-table__content.info-table__content--bold a[href^='/aktuell/waspassiertheute/aktuell/new/datum/']").text.strip()
+    except AttributeError:
+        date_of_birth = None
+    res["date_of_birth"] = date_of_birth
+
+    #Getting Citizenship
+    try:
+        citizenship = soup.select_one("li.data-header__label span[itemprop='nationality']").get_text(strip = True)
+    except AttributeError:
+        citizenship = None
+    res["citizenship"] = citizenship
+
+    #Getting Place Of Birth
+    try:
+        place_of_birth = soup.select_one("li.data-header__label span.data-header__content[itemprop='birthPlace']").get_text(strip = True)
+    except AttributeError:
+        place_of_birth = None
+    res["place_of_birth"] = place_of_birth
+
+    #Getting Caps and Goals
+    try:
+        caps = soup.select_one("li.data-header__label > a:nth-of-type(1).data-header__content--highlight").get_text(strip = True)
+        goals = soup.select_one("li.data-header__label > a:nth-of-type(2).data-header__content--highlight").get_text(strip = True)
+    except AttributeError:
+        caps = None
+        goals = None
+    res["caps"] = caps
+    res["goals"] = goals
+
+    #Getting Player Agent
+    try:
+        agent = soup.select_one("li.data-header__label:contains('Agent') a").get_text(strip = True)
+    except AttributeError:
+        agent = None
+
+    #Getting Other Positions
+    try:
+        other_position_soup = soup.find("div", class_ = "detail-position__position")
+        other_positions = [position.text.strip() for position in other_position_soup.find_all("dd", class_ = "detail-position__position")]
+        other_positions = ", ".join(other_positions)
+    except AttributeError:
+        other_positions = None
+    res["other_positions"] = other_positions
+
+    temp = soup.select("#main > main > div > div.large-8.columns > div > div > div.large-6.large-pull-6.small-12.columns.spielerdatenundfakten > div > span")
+
+    outfitter = None
+    contract_expires = None
+    foot = None
+    contract_Joined = None
+    height = None
+    current_club = None
+    date_of_last_contract = None
+
+    for i in range(len(temp)):
+        #Getting Outfitter
+        if "Outfitter" in temp[i].text:
+            outfitter = temp[i+1].text.strip()
+        #Getting Contract Expires
+        elif "Contract expires" in temp[i].text:
+            contract_expires = temp[i+1].text.strip()
+        #Getting Agent
+        elif "agent" in temp[i].text and agent == None:
+            agent = temp[i+1].text.strip()
+        #Getting Foot
+        elif "Foot" in temp[i].text:
+            foot = temp[i+1].text.strip()
+        #Getting Joined
+        elif "Joined" in temp[i].text:
+            contract_Joined = temp[i+1].text.strip()
+        #Getting Height
+        elif "Height" in temp[i].text:
+            height = temp[i+1].text.strip()
+            height = str("".join(filter(str.isdigit, height)))
+        #Getting Current Club
+        elif "Current club" in temp[i].text:
+            current_club = temp[i+1].text.strip()
+        #Getting last Contract
+        elif "last contract" in temp[i].text:
+            date_of_last_contract = temp[i+1].text.strip()
+
+    res["outfitter"] = outfitter
+    res["contract_expires"] = contract_expires
+    res["agent"] = agent
+    res["foot"] = foot
+    res["contract_Joined"] = contract_Joined
+    res["height"] = height
+    res["current_club"] = current_club
+    res["date_of_last_contract"] = date_of_last_contract
+
+    return res
+
+
+def players_transfer_table(url):       
+    page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    #Getting Transfer Data
+    try:
+        transfer_table = soup.find("div", {"data-viewport" : "Transferhistorie"})
+        rows = transfer_table.find_all("div", class_ = "grid tm-player-transfer-history-grid")
+        transfer_table_list = []
+        for i in rows:
+            clean_text = i.text.strip()
+            lines = clean_text.split("\n")
+            lines = [line.strip() for line in lines if line.strip()]
+            if ("2017" in lines[1]) or ("2018" in lines[1]) or ("2019" in lines[1]) or ("2020" in lines[1]) or ("2021" in lines[1]):
+                transfer_table_list += [lines]
+        return transfer_table_list
+    
+    except AttributeError:
+        pass
+
+
+def player_stats_table(url):
+    page = requests.get(performance_link, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    appearances=np.nan
+    goals=np.nan
+    asists=np.nan
+    yellow_cards=np.nan
+    second_yellow_cards=np.nan
+    red_cards=np.nan
+    minutes_played=np.nan
+    goals_conceded=np.nan
+    clean_sheets=np.nan
+
+    row={}
+    if soup.select('.empty'):
+        pass
+    else:
+        x = soup.select('#yw1 tfoot tr td')
+        if len(x)==9:
+            appearances=x[2].text
+            goals = x[3].text
+            asists = x[4].text
+            yellow_cards = x[5].text
+            second_yellow_cards = x[6].text
+            red_cards = x[7].text
+            minutes_played = x[8].text
+
+        elif len(x)==10:
+            appearances = x[2].text
+            goals = x[3].text
+            yellow_cards = x[4].text
+            second_yellow_cards = x[5].text
+            red_cards = x[6].text
+            goals_conceded = x[7].text
+            clean_sheets = x[8].text
+            minutes_played = x[9].text
+    
+    row={'id':id,
+    'name':name,
+    'appearances':appearances,
+    'goals':goals,
+    'asists':asists,
+    'yellow_cards':yellow_cards,
+    'second_yellow_cards':second_yellow_cards,
+    'red_cards':red_cards,
+    'minutes_played':minutes_played,
+    'goals_conceded':goals_conceded,
+    'clean_sheets':clean_sheets}
+
+    return row
+
+    
+
+links = 'https://www.transfermarkt.com' + pd.read_csv("data/players_link.csv")
+
+cols = ["player_id", "shirt_number", "given_name", "full_name", "date_of_birth",
+        "citizenship", "place_of_birth", "caps", "goals", "other_positions", 
+        "outfitter", "contract_expires", "agent", "foot", "contract_Joined",
+        "height", "current_club", "date_of_last_contract", "players_link"]
+players_datas = pd.DataFrame(columns=cols).astype(str)
+
+cols=["player_id", "season", "date", "left", "joined", "mv", "fee"]
+transfer_table = pd.DataFrame(columns=cols).astype(str)
+
+for i in range(len(links)):
+    #---players_datas
+    res1 = data_of_players(links.loc[i, "0"], players_datas)
+    # players_datas = players_datas.append(res1, ignore_index = True) # for pandas==1.3.4
+    players_datas = pd.concat([players_datas, pd.DataFrame([res1])], ignore_index=True)
+
+    #---players_transfers
+    res2 = {}
+    #Getting Players ID
+    pattern = r"/(\d+)$"
+    match = re.search(pattern, links.loc[i, "0"])
+    player_id = match.group(1)
+    table_list = players_transfer_table(links.loc[i, "0"])
+    
+    for j in table_list:
+        if len(j) == 6:
+            res2["player_id"] = player_id
+            res2["season"] = j[0]
+            res2["date"] = j[1]
+            res2["left"] = j[2]
+            res2["joined"] = j[3]
+            res2["mv"] = j[4]
+            res2["fee"] = j[5]
+            # transfer_table = transfer_table.append(res2, ignore_index = True) # for pandas==1.3.4
+            transfer_table = pd.concat([transfer_table, pd.DataFrame([res2])], ignore_index=True)
+    
+    #---players_stats
+    player_stats = []
+    p=links.iloc[i,0]
+    id=p.split('spieler/')[-1]
+    
+    name=p.split('com/')[-1].split('/profil')[0]
+    name=name.replace(' ','-')
+    performance_link=f"https://www.transfermarkt.com/{name}/leistungsdaten/spieler/{id}/plus/0?saison=2021"
+
+    res3 = player_stats_table(performance_link)
+    player_stats.append(res3)
+
+    print(f'player{i} Done.')
+
+
+
+pd.DataFrame(players_datas).to_csv("data/players_datas.csv", index = False)
+pd.DataFrame(transfer_table).drop_duplicates().to_csv("data/players_transfers.csv", index = False)
+pd.DataFrame(player_stats).to_csv('data/players_stats.csv',index=False)
